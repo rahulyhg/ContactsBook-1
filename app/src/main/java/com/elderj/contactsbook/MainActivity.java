2742a7e8d23a8efed2967c66b0cfe6b76a4fdd38
Jo <parusmontanus@gmail.com>
2017-09-25 12:08:37 +0100

    Setting up activity layout

--- app/src/main/java/com/elderj/contactsbook/MainActivity.java
+++ app/src/main/java/com/elderj/contactsbook/MainActivity.java
@@ -2,12 +2,35 @@ package com.elderj.contactsbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public TextView orgName;
+    public EditText orgNameEdit;
+    public TextView orgEmail;
+    public EditText orgEmailEdit;
+    public TextView orgPhone;
+    public EditText orgPhoneEdit;
+    public Button saveOrg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        orgName = (TextView) findViewById(R.id.org_name);
+        orgNameEdit = (EditText) findViewById(R.id.org_name_edit);
+        orgEmail = (TextView) findViewById(R.id.org_email);
+        orgEmailEdit = (EditText) findViewById(R.id.org_email_edit);
+        orgPhone = (TextView) findViewById(R.id.org_phone);
+        orgPhoneEdit = (EditText) findViewById(R.id.org_phone_edit);
+        saveOrg = (Button) findViewById(R.id.save_org);
+
     }
+
+
+
 }
